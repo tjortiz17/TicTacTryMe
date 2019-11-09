1569d23490c9ebcdec95b2d2eaec282300d7977e
tjortiz17 <tjortiz17@gmail.com>
2019-11-09 11:43:29 -0800

    initial commit

---
+++ app/src/main/java/com/example/tictactryme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tictactryme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //button click method v1, just show an X on click
+    public void makeMove(View view){
+        
+    }
+}

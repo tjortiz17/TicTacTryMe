432b674d9ee2aeaa15aa5a5adb698af3e8f7a16f
tjortiz17 <tjortiz17@gmail.com>
2019-11-09 14:33:10 -0800

    added functionality to check win/tie conditions

--- app/src/main/java/com/example/tictactryme/MainActivity.java
+++ app/src/main/java/com/example/tictactryme/MainActivity.java
@@ -9,10 +9,17 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[][] moves = new String[3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                moves[i][j] = " ";
+            }
+        }
     }
 
     boolean isX = true;
@@ -21,135 +28,254 @@ public class MainActivity extends AppCompatActivity {
     public void makeMove1(View view){
         TextView textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button1);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[0][0] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[0][0] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove2(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button2);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[0][1] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[0][1] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove3(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button3);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[0][2] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[0][2] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove4(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button4);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[1][0] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[1][0] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove5(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button5);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[1][1] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[1][1] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove6(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button6);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[1][2] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[1][2] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove7(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button7);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[2][0] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[2][0] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove8(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button8);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[2][1] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[2][1] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
     }
     public void makeMove9(View view){
         TextView textView = findViewById(R.id.textView);
         //textView.setText("Player O's turn");
         Button button = findViewById(R.id.button9);
-
-        if(isX){
-            button.setText("X");
-            textView.setText("Player O's turn");
-        }
-        else{
-            button.setText("0");
-            textView.setText("Player X's turn");
+        if(!isGameOver() && !isWin()) {
+            if (isX) {
+                button.setText("X");
+                textView.setText("Player O's turn");
+                moves[2][2] = "X";
+            } else {
+                button.setText("O");
+                textView.setText("Player X's turn");
+                moves[2][2] = "O";
+            }
         }
         isX = !isX;
+        isGameOver();
+        isWin();
+    }
+    //returns true if board is full only
+    public boolean isGameOver(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(moves[i][j].equals(" ")){
+                    return false;
+                }
+            }
+        }
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Game Over. You Tied");
+        return true;
+    }
+
+    //checks for win condition for either player
+    public boolean isWin(){
+        TextView textView = findViewById(R.id.textView);
+        if(moves[0][0].equals("X") && moves[0][1].equals("X") && moves[0][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[1][0].equals("X") && moves[1][1].equals("X") && moves[1][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[2][0].equals("X") && moves[2][1].equals("X") && moves[2][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[0][0].equals("X") && moves[1][0].equals("X") && moves[2][0].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[0][1].equals("X") && moves[1][1].equals("X") && moves[2][1].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[0][2].equals("X") && moves[1][2].equals("X") && moves[2][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[0][0].equals("X") && moves[1][1].equals("X") && moves[2][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[2][0].equals("X") && moves[1][1].equals("X") && moves[0][2].equals("X")){
+            textView.setText("Player X Wins!");
+            return true;
+        }
+        else if(moves[0][0].equals("O") && moves[0][1].equals("O") && moves[0][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[1][0].equals("O") && moves[1][1].equals("O") && moves[1][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[2][0].equals("O") && moves[2][1].equals("O") && moves[2][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[0][0].equals("0") && moves[1][0].equals("O") && moves[2][0].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[0][1].equals("O") && moves[1][1].equals("O") && moves[2][1].equals("O")){
+            textView.setText("Player 0 Wins!");
+            return true;
+        }
+        else if(moves[0][2].equals("O") && moves[1][2].equals("O") && moves[2][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[0][0].equals("O") && moves[1][1].equals("O") && moves[2][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        else if(moves[2][0].equals("O") && moves[1][1].equals("O") && moves[0][2].equals("O")){
+            textView.setText("Player O Wins!");
+            return true;
+        }
+        return false;
     }
 }
